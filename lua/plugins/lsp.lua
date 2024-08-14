b627f90415dfdb432a39fd78ef0c77bf8c80cb77
Dsaquel <noblet.ouwaysgta5@gmail.com>
2024-08-15 00:03:32 +0200

    initial commit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,132 @@
+return {
+	-- tools
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"prettierd",
+				"stylua",
+				"selene",
+				"luacheck",
+				"shellcheck",
+				"shfmt",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"vue-language-server",
+				"css-lsp",
+				"python-lsp-server",
+			})
+		end,
+	},
+
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		init = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			keys[#keys + 1] = {
+				"gd",
+				function()
+					-- DO NOT RESUSE WINDOW
+					require("telescope.builtin").lsp_definitions({ reuse_win = false })
+				end,
+				desc = "Goto Definition",
+				has = "definition",
+			}
+		end,
+		opts = {
+			inlay_hints = { enabled = false },
+			---@type lspconfig.options
+			servers = {
+				cssls = {},
+				tailwindcss = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+				},
+				volar = {
+					filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+					init_options = {
+						vue = {
+							hybridMode = false,
+						},
+					},
+				},
+				html = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							keyOrdering = false,
+						},
+					},
+				},
+				lua_ls = {
+					-- enabled = false,
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								workspaceWord = true,
+								callSnippet = "Both",
+							},
+							misc = {
+								parameters = {
+									-- "--log-level=trace",
+								},
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							type = {
+								castNumberToInteger = true,
+							},
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								-- enable = false,
+								groupSeverity = {
+									strong = "Warning",
+									strict = "Warning",
+								},
+								groupFileStatus = {
+									["ambiguity"] = "Opened",
+									["await"] = "Opened",
+									["codestyle"] = "None",
+									["duplicate"] = "Opened",
+									["global"] = "Opened",
+									["luadoc"] = "Opened",
+									["redefined"] = "Opened",
+									["strict"] = "Opened",
+									["strong"] = "Opened",
+									["type-check"] = "Opened",
+									["unbalanced"] = "Opened",
+									["unused"] = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = {
+								enable = false,
+								defaultConfig = {
+									indent_style = "space",
+									indent_size = "2",
+									continuation_indent_size = "2",
+								},
+							},
+						},
+					},
+				},
+			},
+			setup = {},
+		},
+	},
+}
